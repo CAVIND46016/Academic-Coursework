f193e5aace448e6ff4bef6f1be86393f9f0e5536
Cavin Dsouza <cavin46016@gmail.com>
2017-02-03 02:48:41 -0500

    Create test_sort.py

---
+++ Analysis of Sorting Algorithms/test_sort.py
@@ -0,0 +1,30 @@
+import sorting
+import random
+import time
+
+tmp = [];
+length = 25000;
+for x in range(length):
+    tmp.append(random.randint(-9999, 9999));
+
+print("Sample array");
+print(tmp)
+
+s_t = time.time(); 
+b1 = sorting.insertion_sort(tmp)
+print("Insertion sort: {} secs".format(time.time() - s_t))
+
+s_t = time.time();
+b2 = sorting.quick_sort(tmp)
+print("Quick sort: {} secs".format(time.time() - s_t))
+
+s_t = time.time();
+b3 = sorting.bubble_sort(tmp)
+print("Bubble sort: {} secs".format(time.time() - s_t))
+
+s_t = time.time();
+b4 = sorting.merge_sort(tmp)
+print("Merge sort: {} secs".format(time.time() - s_t))
+
+assert(b1 == b2 == b3 == b4)
+print("b1 == b2 == b3 == b4"); #Ensuring array sorting correctness

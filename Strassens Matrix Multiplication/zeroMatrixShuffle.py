a7759e25868a6cef57e64d8315f19891543e5d78
Cavin Dsouza <cavin46016@gmail.com>
2018-03-21 05:56:31 -0400

    Update zeroMatrixShuffle.py

--- Strassens Matrix Multiplication/zeroMatrixShuffle.py
+++ Strassens Matrix Multiplication/zeroMatrixShuffle.py
@@ -1,27 +1,26 @@
-#Cavin Dsouza
-#dsouzac@indiana.edu
-#Python version 3.5.2
+"""
+Accepts two test matrices, varies the percentage of zeros in it as per user 
+specification and writes it into two text files ==> 'matReadZERO1.txt' and 
+'matReadZERO2.txt'.
+"""
+import random
 
-#Accepts two test matrices, varies the percentage of zeros in it as per user specification and 
-#writes it into two text files ==> 'matReadZERO1.txt' and 'matReadZERO2.txt'
-#Directory created: C:\Workspace
+MATRIXINPUT1 = 'matRead1.txt'
+MATRIXINPUT2 = 'matRead2.txt'
 
-import random;
-
-MATRIXINPUT1 = 'C:\\Workspace\\matRead1.txt'
-MATRIXINPUT2 = 'C:\\Workspace\\matRead2.txt'
-
-MATRIXOUTPUT1 = 'C:\\Workspace\\matReadZERO1.txt'
-MATRIXOUTPUT2 = 'C:\\Workspace\\matReadZERO2.txt'
+MATRIXOUTPUT1 = 'matReadZERO1.txt'
+MATRIXOUTPUT2 = 'matReadZERO2.txt'
 
 def shuffle2d(arr2d, rand=random):
-    #***************************************************************************************
-    #*    Title: How do I shuffle a multidimensional list in Python
-    #*    Author: Mike Housky
-    #*    Date: 12/07/2014
-    #*    Availability: http://stackoverflow.com/questions/27337784/how-do-i-shuffle-a-multidimensional-list-in-python
-    #***************************************************************************************
-    #Shuffes entries of 2-d array arr2d, preserving shape.
+    """********************************************************************
+    *    Title: How do I shuffle a multidimensional list in Python
+    *    Author: Mike Housky
+    *    Date: 12/07/2014
+    *    Availability: http://stackoverflow.com/questions/27337784/how-do\
+                        -i-shuffle-a-multidimensional-list-in-python
+    ***********************************************************************
+    Shuffes entries of 2-d array arr2d, preserving shape.
+    """
     reshape = []
     data = []
     iend = 0
@@ -30,54 +29,58 @@ def shuffle2d(arr2d, rand=random):
         istart, iend = iend, iend+len(row)
         reshape.append((istart, iend))
     rand.shuffle(data)
-    return [data[istart:iend] for (istart,iend) in reshape]
+    return [data[istart:iend] for (istart, iend) in reshape]
+    
+def zero_matrix(_filename, mat_dim1, mat_dim2, perc):
+    """
+    Assigns a 'perc'% of elements of the 
+    mat_dim1 * mat_dim2 matrix to 0
+    """
+    ctr = 0
+    nzeros = int(perc / 100 * (mat_dim1 * mat_dim2)) 
+    num = []
+    
+    with open(_filename, 'r') as _file:
+        for each_line in _file:
+            line = [int(x) for x in each_line.split(',')]
+            num.append(line)
+    
+    for i in range(mat_dim1):
+        for j in range(mat_dim2):
+            num[i][j] = 0
+            ctr += 1
+            if ctr == nzeros:
+                break
+        if ctr == nzeros:
+            break
+        
+    return shuffle2d(num)
 
-def display_Matrix(arr2d):
-    print("\n".join(str(row) for row in arr2d))
-    
-def zero_Matrix(_filename, _matDim1, _matDim2, _percent):
-    f = open(_filename, 'r')
-    counter = 0;
-    nzeros = int(_percent / 100 * (_matDim1 * _matDim2)); 
-    
-    numbers = []
-    for eachLine in f:
-        line = [int(x) for x in eachLine.split(',')]
-        numbers.append(line)
-    f.close();
-    
-    for i in range(0, _matDim1):
-        for j in range(0, _matDim2):
-            numbers[i][j] = 0;
-            counter += 1;
-            if(counter == nzeros):
-                break;
-        if(counter == nzeros):
-            break; 
-    return (shuffle2d(numbers));
+def write_matrix_to_file(_filename, _result):
+    """
+    Writes matrix to file.
+    """
+    with open(_filename, 'w') as _file:
+        for i in range(len(_result)):
+            for j in range(len(_result[0])):
+                if j != len(_result[0]) - 1:
+                    _file.write(str(_result[i][j]) + ",")
+                else:
+                    _file.write(str(_result[i][j]))
+            _file.write('\n')
+  
+def main():  
+    """
+    Entry-point for the function.
+    """
+    rows1, columns1 = 512, 512
+    rows2, columns2 = 512, 512
 
-def write_Matrix_to_file(_filename, _result):
-    f = open(_filename, 'w');
-    for i in range(len(_result)):
-        for j in range(len(_result[0])):
-            if(j != len(_result[0]) - 1):
-                f.write(str(_result[i][j]) + ",");
-            else:
-                f.write(str(_result[i][j]));
-        f.write('\n')
-    f.close();
+    perc = 75
     
-def main():
-    rows1       = 512;
-    columns1    = 512;
-
-    rows2       = 512;
-    columns2    = 512;
-    
-    percentage  = 75;
-    
-    write_Matrix_to_file(MATRIXOUTPUT1, zero_Matrix(MATRIXINPUT1, rows1, columns1, percentage));
-    write_Matrix_to_file(MATRIXOUTPUT2, zero_Matrix(MATRIXINPUT2, rows2, columns2, percentage));
+    write_matrix_to_file(MATRIXOUTPUT1, zero_matrix(MATRIXINPUT1, rows1, columns1, perc))
+    write_matrix_to_file(MATRIXOUTPUT2, zero_matrix(MATRIXINPUT2, rows2, columns2, perc))
     
 if __name__ == "__main__":
-    main();
+    main()
+    

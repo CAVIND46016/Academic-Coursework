a7c83ad68950d8fe1623f8871e3a1bffd79c79af
Cavin Dsouza <cavin46016@gmail.com>
2018-03-21 09:15:39 -0400

    Update test_sort.py

--- Analysis of Sorting Algorithms/test_sort.py
+++ Analysis of Sorting Algorithms/test_sort.py
@@ -1,49 +1,59 @@
+"""
+Test script that implements sorting algorithms defined
+in sorting.py
+"""
 import sorting
 import random
 import time
 import copy
 
-def printResults(length, sort_case):     
-    temp = [];
-    for x in range(length):
-        temp.append(random.randint(-9999, 9999));
+def print_results(length, sort_case):
+    """
+    Implements sorting definitions on array of given 'length'
+    in best, average or worst case.
+    """     
+    temp = []
+    for _ in range(length):
+        temp.append(random.randint(-9999, 9999))
        
-    if(sort_case == -1): #Worst Case
-        sorting.descendingOrder(temp); #taking as input an array that is reverse sorted
-    elif(sort_case == 1): #Best case
-        sorting.quick_sort(temp); #taking a sorted array as input
+    if sort_case == -1: #Worst Case
+        sorting.descendingOrder(temp) #taking as input an array that is reverse sorted
+    elif sort_case == 1: #Best case
+        sorting.quick_sort(temp) #taking a sorted array as input
   
-    tmp = copy.deepcopy(temp);
-    
-    s_t = time.time(); 
+    tmp = copy.deepcopy(temp)
+    s_t = time.time() 
     sorting.insertion_sort(tmp)
     print("Insertion sort: {} secs".format(time.time() - s_t))
     
-    tmp = copy.deepcopy(temp); 
-    s_t = time.time();
+    tmp = copy.deepcopy(temp)
+    s_t = time.time()
     sorting.quick_sort(tmp)
     print("Quick sort: {} secs".format(time.time() - s_t))
     
-    tmp = copy.deepcopy(temp);
-    s_t = time.time();
+    tmp = copy.deepcopy(temp)
+    s_t = time.time()
     sorting.bubble_sort(tmp)
     print("Bubble sort: {} secs".format(time.time() - s_t))
     
-    tmp = copy.deepcopy(temp);
-    s_t = time.time();
+    tmp = copy.deepcopy(temp)
+    s_t = time.time()
     sorting.bubble_sort_opt(tmp)
     print("Bubble sort (optimized): {} secs".format(time.time() - s_t))
     
-    tmp = copy.deepcopy(temp);
-    s_t = time.time();
+    tmp = copy.deepcopy(temp)
+    s_t = time.time()
     sorting.merge_sort(tmp)
     print("Merge sort: {} secs".format(time.time() - s_t))
     
 def main():
+    """
+    Entry-point for the function.
+    """
     # Worst case = -1, Avg. case = 0, Best Case = 1
     sort_case = -1
     length = 25000
-    printResults(length, sort_case);
+    print_results(length, sort_case)
 
-if(__name__ == "__main__"):
-    main();
+if __name__ == "__main__":
+    main()

53e450d22d0bbbf81488f472f1f879f63727ae69
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2018-08-22 05:17:34 -0400

    Update solver.py

--- Artificial Intelligence/MDP Planning Algorithm/solver.py
+++ Artificial Intelligence/MDP Planning Algorithm/solver.py
@@ -1,31 +1,31 @@
-import random
-
 class Solver:
     def __init__(self, mdp):
-        self.mdp            = mdp;
-        self.stateSpaces    = mdp.S();
-        self.actions        = mdp.A();
-        self.policy         = [];
-        self.gamma          = mdp.gamma();
+        self.mdp = mdp
+        self.state_spaces = mdp.S()
+        self.actions = mdp.A()
+        self.policy = []
+        self.gamma = mdp.gamma()
 
-    #Returns the optimal policy for the MDP as a dictionary from states to actions
     def solve(self):
-        self.policy = [0 for s in range(len(self.stateSpaces))]
-        U = [0 for s in range(len(self.stateSpaces))]
+        self.policy = [0 for s in range(len(self.state_spaces))]
+        U = self.policy[:]
 
-        continue_loop = True;
+        continue_loop = True
         while continue_loop:
             continue_loop = False
-            for s in self.stateSpaces:
-                U[s] = sum([self.mdp.P(s,self.policy[s], s1) * (self.mdp.R(s) + self.gamma * U[s1]) for s1 in self.stateSpaces])
+            for s in self.state_spaces:
+                U[s] = sum([self.mdp.P(s, self.policy[s], s1) \
+                            * (self.mdp.R(s) + self.gamma * U[s1]) for s1 in self.state_spaces])
 
-            for s in self.stateSpaces:
+            for s in self.state_spaces:
                 max_val = U[s]
                 for a in self.actions:
-                    q1 = sum([self.mdp.P(s, a, s1) * (self.mdp.R(s) + self.gamma * U[s1]) for s1 in self.stateSpaces])
+                    q1 = sum([self.mdp.P(s, a, s1) \
+                              * (self.mdp.R(s) + self.gamma * U[s1]) for s1 in self.state_spaces])
                     if q1 > max_val:
-                        self.policy[s] = a;
+                        self.policy[s] = a
                         max_val = q1
-                        continue_loop = True;
+                        continue_loop = True
 
         return self.policy
+    

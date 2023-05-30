88b22b6e9ef9e3ec88574fa5e1c2b63557b4dd92
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2023-05-30 03:12:55 -0400

    Update test.py

--- Artificial Intelligence/MDP Planning Algorithm/test.py
+++ Artificial Intelligence/MDP Planning Algorithm/test.py
@@ -1,49 +1,82 @@
 import solver
 
+
 class ChainMDP:
-    #Returns the probability of transitioning from state s to state u when taking action a
-    def P(self, s, a, u):
-        if a == 'left' and u == s-1:
+
+    @staticmethod
+    def p(s, a, u):
+        """
+        Returns the probability of transitioning from state s to state u when taking action a
+        :param s:
+        :param a:
+        :param u:
+        :return:
+        """
+
+        if a == "left" and u == s - 1:
             return 0.7
-        if a == 'left' and u == s+1:
+        if a == "left" and u == s + 1:
             return 0.3
-        if a == 'left' and s == 0 and u == 0:
+        if a == "left" and s == 0 and u == 0:
             return 0.7
-        if a == 'right' and u == s+1:
+        if a == "right" and u == s + 1:
             return 0.7
-        if a == 'right' and u == s-1:
+        if a == "right" and u == s - 1:
             return 0.3
-        if a == 'right' and u == s and u == 10:
+        if a == "right" and u == s and u == 10:
             return 0.7
         return 0.0
-   
-    #Returns the discount factor that you can assume is in (0,1).   
-    def gamma(self):
+
+    @staticmethod
+    def gamma():
+        """
+        Returns the discount factor that you can assume is in (0,1).
+        :return:
+        """
+
         return 0.9
-  
-    #Accepts a state and returns the reward for visiting that state
-    def R(self, s):
-        if s == 0 or s == 10:
+
+    @staticmethod
+    def r(y):
+        """
+        Accepts a state and returns the reward for visiting that state
+        :param y:
+        :return:
+        """
+
+        if y == 0 or y == 10:
             return 1.0
         return 0.0
 
-    #Returns a finite set of actions, which you can assume will be hashable
-    def A(self):
-        return ['left', 'right']
+    @staticmethod
+    def finite_actions():
+        """
+        Returns a finite set of actions, which you can assume will be hashable
+        :return:
+        """
 
-    #Returns a finite set of states, which you can assume will be hashable
-    def S(self):
-        return [0,1,2,3,4,5,6,7,8,9,10]
+        return ["left", "right"]
 
-    def initial_state(self):
+    @staticmethod
+    def finite_states():
+        """
+        Returns a finite set of states, which you can assume will be hashable
+        :return:
+        """
+
+        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    @staticmethod
+    def initial_state():
         return 5
 
+
 mdp = ChainMDP()
-s = solver.Solver(mdp)
-policy = s.solve()
+solve = solver.Solver(mdp)
+policy = solve.solve()
 
 for i in range(0, 5):
-    assert(policy[i] == 'left')
- 
-for i in range(6,11):
-    assert(policy[i] == 'right')
+    assert (policy[i] == "left")
+
+for i in range(6, 11):
+    assert (policy[i] == "right")

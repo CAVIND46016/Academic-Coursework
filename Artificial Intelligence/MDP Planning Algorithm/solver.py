2b824eb255b35b1099053eb85dd4da917034d489
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2023-05-30 03:12:18 -0400

    Update solver.py

--- Artificial Intelligence/MDP Planning Algorithm/solver.py
+++ Artificial Intelligence/MDP Planning Algorithm/solver.py
@@ -1,31 +1,38 @@
 class Solver:
     def __init__(self, mdp):
         self.mdp = mdp
-        self.state_spaces = mdp.S()
-        self.actions = mdp.A()
+        self.state_spaces = mdp.finite_states()
+        self.actions = mdp.finite_actions()
         self.policy = []
         self.gamma = mdp.gamma()
 
     def solve(self):
-        self.policy = [0 for s in range(len(self.state_spaces))]
-        U = self.policy[:]
+        self.policy = [0 for _ in range(len(self.state_spaces))]
+        u = self.policy[:]
 
         continue_loop = True
         while continue_loop:
             continue_loop = False
             for s in self.state_spaces:
-                U[s] = sum([self.mdp.P(s, self.policy[s], s1) \
-                            * (self.mdp.R(s) + self.gamma * U[s1]) for s1 in self.state_spaces])
+                u[s] = sum(
+                    [
+                        self.mdp.p(s, self.policy[s], s1) * (self.mdp.r(s) + self.gamma * u[s1])
+                        for s1 in self.state_spaces
+                    ]
+                )
 
             for s in self.state_spaces:
-                max_val = U[s]
+                max_val = u[s]
                 for a in self.actions:
-                    q1 = sum([self.mdp.P(s, a, s1) \
-                              * (self.mdp.R(s) + self.gamma * U[s1]) for s1 in self.state_spaces])
+                    q1 = sum(
+                        [
+                            self.mdp.p(s, a, s1) * (self.mdp.r(s) + self.gamma * u[s1])
+                            for s1 in self.state_spaces
+                        ]
+                    )
                     if q1 > max_val:
                         self.policy[s] = a
                         max_val = q1
                         continue_loop = True
 
         return self.policy
-    

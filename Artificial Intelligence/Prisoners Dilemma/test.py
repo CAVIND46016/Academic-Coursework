5a6e4b1f3cbd209e37f579818473f2fd0bd7d5ab
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2023-05-30 21:07:04 -0400

    Update test.py

--- Artificial Intelligence/Prisoners Dilemma/test.py
+++ Artificial Intelligence/Prisoners Dilemma/test.py
@@ -1,7 +1,7 @@
-import alice
+from alice import Alice
 import random
 
-a = alice.Alice()
+a = Alice()
 print(a.name())
 
 N = 100
@@ -11,10 +11,10 @@ utility = 0
 for i in range(N):
     action_alice = a.get_action()
     action_bob = "BETRAY" if random.random() < 0.5 else "SILENT"
-    a.give_action(action_bob); #Updates bob's action to opponent history list.
-    
-    assert(action_alice in ["BETRAY", "SILENT"])
-  
+    a.give_action(action_bob)  # Updates Bob's action to opponent history list.
+
+    assert action_alice in ["BETRAY", "SILENT"]
+
     if action_alice == "BETRAY" and action_bob == "BETRAY":
         utility -= 2
     if action_alice == "SILENT" and action_bob == "BETRAY":
@@ -24,6 +24,6 @@ for i in range(N):
     if action_alice == "SILENT" and action_bob == "SILENT":
         utility -= 1
 
-a.game_over()
+Alice.game_over()
 
 print("utility was: " + str(utility))

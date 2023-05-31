7f21928936f77baf827fe7567390abb6d1f1d724
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2023-05-30 22:28:57 -0400

    Update zero_matrix_shuffle.py

--- Strassens Matrix Multiplication/zero_matrix_shuffle.py
+++ Strassens Matrix Multiplication/zero_matrix_shuffle.py
@@ -1,86 +1,100 @@
 """
-Accepts two test matrices, varies the percentage of zeros in it as per user 
-specification and writes it into two text files ==> 'matReadZERO1.txt' and 
+Accepts two test matrices, varies the percentage of zeros in it as per user
+specification and writes it into two text files ==> 'matReadZERO1.txt' and
 'matReadZERO2.txt'.
 """
 import random
 
-MATRIXINPUT1 = 'matRead1.txt'
-MATRIXINPUT2 = 'matRead2.txt'
+MATRIX_INPUT_1 = "matRead1.txt"
+MATRIX_INPUT_2 = "matRead2.txt"
 
-MATRIXOUTPUT1 = 'matReadZERO1.txt'
-MATRIXOUTPUT2 = 'matReadZERO2.txt'
+MATRIX_OUTPUT_1 = "matReadZERO1.txt"
+MATRIX_OUTPUT_2 = "matReadZERO2.txt"
 
-def shuffle2d(arr2d, rand=random):
-    """********************************************************************
-    *    Title: How do I shuffle a multidimensional list in Python
-    *    Author: Mike Housky
-    *    Date: 12/07/2014
-    *    Availability: http://stackoverflow.com/questions/27337784/how-do\
-                        -i-shuffle-a-multidimensional-list-in-python
-    ***********************************************************************
-    Shuffes entries of 2-d array arr2d, preserving shape.
+
+def shuffle2d(arr):
     """
+    Shuffles entries of 2-d array, preserving shape.
+    Title: How do I shuffle a multidimensional list in Python
+    Author: Mike Housky
+    Date: 12/07/2014
+    Availability: http://stackoverflow.com/questions/27337784/how-do-i-shuffle-a-multidimensional-list-in-python
+    Shuffles entries of 2-d array, preserving shape.
+    :param arr:
+    :return:
+    """
+
     reshape = []
     data = []
-    iend = 0
-    for row in arr2d:
+    end = 0
+    for row in arr:
         data.extend(row)
-        istart, iend = iend, iend+len(row)
-        reshape.append((istart, iend))
-    rand.shuffle(data)
-    return [data[istart:iend] for (istart, iend) in reshape]
-    
+        istart, end = end, end + len(row)
+        reshape.append((istart, end))
+    random.shuffle(data)
+    return [data[istart:end] for (istart, end) in reshape]
+
+
 def zero_matrix(_filename, mat_dim1, mat_dim2, perc):
     """
-    Assigns a 'perc'% of elements of the 
-    mat_dim1 * mat_dim2 matrix to 0
+    Assigns a subset of the mat_dim1 * mat_dim2 matrix to 0
+    :param _filename:
+    :param mat_dim1:
+    :param mat_dim2:
+    :param perc:
+    :return:
     """
+
     ctr = 0
-    nzeros = int(perc / 100 * (mat_dim1 * mat_dim2)) 
+    n_zeros = int(perc / 100 * (mat_dim1 * mat_dim2))
     num = []
-    
-    with open(_filename, 'r') as _file:
+
+    with open(_filename, "r", encoding="utf8") as _file:
         for each_line in _file:
-            line = [int(x) for x in each_line.split(',')]
+            line = [int(x) for x in each_line.split(",")]
             num.append(line)
-    
+
     for i in range(mat_dim1):
         for j in range(mat_dim2):
             num[i][j] = 0
             ctr += 1
-            if ctr == nzeros:
+            if ctr == n_zeros:
                 break
-        if ctr == nzeros:
+        if ctr == n_zeros:
             break
-        
+
     return shuffle2d(num)
 
-def write_matrix_to_file(_filename, _result):
+
+def write_matrix_to_file(filename, result):
     """
     Writes matrix to file.
+    :param filename:
+    :param result:
+    :return:
     """
-    with open(_filename, 'w') as _file:
-        for i in range(len(_result)):
-            for j in range(len(_result[0])):
-                if j != len(_result[0]) - 1:
-                    _file.write(str(_result[i][j]) + ",")
-                else:
-                    _file.write(str(_result[i][j]))
-            _file.write('\n')
-  
-def main():  
-    """
-    Entry-point for the function.
-    """
+
+    with open(filename, "w", encoding="utf8") as file:
+        for row in result:
+            file.write(",".join(str(element) for element in row))
+            file.write("\n")
+
+
+def main():
     rows1, columns1 = 512, 512
     rows2, columns2 = 512, 512
 
     perc = 75
-    
-    write_matrix_to_file(MATRIXOUTPUT1, zero_matrix(MATRIXINPUT1, rows1, columns1, perc))
-    write_matrix_to_file(MATRIXOUTPUT2, zero_matrix(MATRIXINPUT2, rows2, columns2, perc))
-    
+
+    write_matrix_to_file(
+        MATRIX_OUTPUT_1,
+        zero_matrix(MATRIX_INPUT_1, rows1, columns1, perc)
+    )
+    write_matrix_to_file(
+        MATRIX_OUTPUT_2,
+        zero_matrix(MATRIX_INPUT_2, rows2, columns2, perc)
+    )
+
+
 if __name__ == "__main__":
     main()
-    
